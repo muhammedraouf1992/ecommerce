6ad14e5233d3bbe538ef82e2350a30deb7c75d23
muhammed <muhammedraouf92@gmail.com>
2023-09-07 16:57:14 +0300

    edit

--- react/src/layout/frontend/pages/SingleProduct.jsx
+++ react/src/layout/frontend/pages/SingleProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosClient from "../../../axios";
-
+import Col from "react-bootstrap/esm/Col";
 const SingleProduct = () => {
     const params = useParams();
 
@@ -26,11 +26,38 @@ const SingleProduct = () => {
     }
 
     return (
-        <div>
-            <h1>{fetchData.title}</h1>
-            <h1>{fetchData.original_price}</h1>
-            <h1>{fetchData.selling_price}</h1>
-        </div>
+        <>
+            <Col lg={5} md={6} sm={12}>
+                <img
+                    src={`http://127.0.0.1:8000/${fetchData.image}`}
+                    alt=""
+                    width={"100%"}
+                    className="rounded"
+                />
+            </Col>
+            <Col lg={7} md={6} sm={12}>
+                <div>
+                    <h1 className="text-capitalize">{fetchData.title}</h1>
+                    <p className="text-capitalize">{fetchData.description}</p>
+                </div>
+                <div>
+                    <span className="mr-5 text-danger fs-4">
+                        <s>{fetchData.original_price}$</s>
+                    </span>
+                    <span className="fs-4">{fetchData.selling_price}$</span>
+                </div>
+                <div>
+                    <span className="py-2 px-4 bg-success text-white mt-2 d-inline-block text-uppercase">
+                        in stock
+                    </span>
+                </div>
+                <div>
+                    <button className="btn btn-danger mt-5">
+                        Add to wishlist
+                    </button>
+                </div>
+            </Col>
+        </>
     );
 };
 
