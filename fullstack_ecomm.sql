17a5f17a530d951276157dac96ed02ff86fdab19
muhammedraouf1992 <121482381+muhammedraouf1992@users.noreply.github.com>
2023-09-26 23:45:38 +0300

    replace the db

--- fullstack_ecomm.sql
+++ fullstack_ecomm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 09, 2023 at 11:15 PM
+-- Generation Time: Sep 26, 2023 at 10:44 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -36,6 +36,14 @@ CREATE TABLE `carts` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `carts`
+--
+
+INSERT INTO `carts` (`id`, `user_id`, `product_id`, `product_quantity`, `created_at`, `updated_at`) VALUES
+(11, 2, 11, 1, NULL, NULL),
+(12, 3, 15, 2, NULL, '2023-09-26 17:42:03');
+
 -- --------------------------------------------------------
 
 --
@@ -61,9 +69,12 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `title`, `slug`, `description`, `image`, `meta_title`, `meta_keyword`, `meta_description`, `status`, `created_at`, `updated_at`) VALUES
-(7, 'men', 'Slug', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus officia non dolor minima nobis ut.', NULL, NULL, NULL, NULL, 0, '2023-09-02 10:13:27', '2023-09-02 11:09:28'),
-(10, 'mobile', 'mobile', 'Description', NULL, 'Meta Title', 'Meta Keyword', 'Meta Description', 0, '2023-09-02 14:45:05', '2023-09-02 14:45:05'),
-(11, 'women', 'women', 'Description', NULL, 'Meta Title', 'Meta Keyword', 'Meta Description', 0, '2023-09-02 14:45:27', '2023-09-02 14:45:27');
+(7, 'jeans', 'jeans', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus officia non dolor minima nobis ut.', 'uploads/categories/64ff851e182b6.jpg', 'Meta Title', 'Meta Keyword', 'Meta Description', 0, '2023-09-02 10:13:27', '2023-09-11 18:22:38'),
+(10, 't-shirts', 't-shirts', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt maiores quo distinctio nobis adipisci fugit?', 'uploads/categories/64ff856395ba9.jpg', 'Meta Title', 'Meta Keyword', 'Meta Description', 0, '2023-09-02 14:45:05', '2023-09-11 18:23:47'),
+(11, 'Shirts', 'Shirts', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex eos unde odio adipisci, facere dignissimos et minima architecto atque quidem!', 'uploads/categories/64ff84d3d753a.jpg', 'Meta Title', 'Meta Keyword', 'Meta Description', 0, '2023-09-02 14:45:27', '2023-09-11 18:21:23'),
+(15, 'sports', 'sports', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem in fugiat maiores.', 'uploads/categories/64ff8621d09dd.jpg', 'Meta Title', 'Meta Keyword', 'Meta Description', 0, '2023-09-11 18:26:57', '2023-09-11 18:26:57'),
+(16, 'sofa', 'sofa', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, sed, aliquam cupiditate labore maiores aperiam architecto assumenda qui illum amet possimus itaque.', 'uploads/categories/64ff88496bc97.jpg', 'Meta Title', 'Meta Keyword', 'Meta Description', 0, '2023-09-11 18:36:09', '2023-09-11 18:36:09'),
+(17, 'tv', 'tv', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, officiis. Error, dolore.', 'uploads/categories/64ff7b502dec6.jpg', 'Meta Title', 'Meta Keyword', 'Meta Description', 0, '2023-09-11 17:40:48', '2023-09-11 17:40:48');
 
 -- --------------------------------------------------------
 
@@ -134,13 +145,6 @@ CREATE TABLE `orders` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`id`, `user_id`, `first_name`, `last_name`, `phone`, `email`, `address`, `city`, `state`, `zip_code`, `payment_id`, `payment_method`, `tracking_number`, `remark`, `status`, `created_at`, `updated_at`) VALUES
-(4, 2, 'a', 'a', 'a', 'admin@admin.com', 'a', 'a', 'a', 'a', NULL, 'COD', 'ecom64fcd2e16c8b2', NULL, 0, '2023-09-09 17:17:37', '2023-09-09 17:17:37');
-
 -- --------------------------------------------------------
 
 --
@@ -156,13 +160,6 @@ CREATE TABLE `order_items` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `order_items`
---
-
-INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `product_price`, `product_quantity`, `created_at`, `updated_at`) VALUES
-(7, 4, 3, 120, 3, '2023-09-09 17:17:37', '2023-09-09 17:17:37');
 
 -- --------------------------------------------------------
 
@@ -203,7 +200,8 @@ INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `n
 (4, 'App\\Models\\User', 2, 'main', 'e3b4fe09f113c7c6b5c40083a89ff6c18c6dbd2a78a5bbf364dd63d023bea800', '[\"*\"]', NULL, NULL, '2023-09-01 19:02:36', '2023-09-01 19:02:36'),
 (8, 'App\\Models\\User', 2, 'main', 'ea072d49d5eed708a5f5dec41a95619200c68ef1959c0b815f16607f9fa9d5cd', '[\"*\"]', NULL, NULL, '2023-09-01 19:12:59', '2023-09-01 19:12:59'),
 (10, 'App\\Models\\User', 3, 'main', 'ba3dd54e98838f6f9ce1ba2ba9411ab9ecfcbb1deb9324410206a0f0c012db76', '[\"*\"]', NULL, NULL, '2023-09-02 06:30:09', '2023-09-02 06:30:09'),
-(25, 'App\\Models\\User', 2, 'main', 'fb33784d24b337a2b9f8a101dc9b1f38a058bfdeae444bdeb4a2c4c9f59b407e', '[\"*\"]', '2023-09-09 17:31:54', NULL, '2023-09-09 10:40:33', '2023-09-09 17:31:54');
+(32, 'App\\Models\\User', 3, 'main', 'cc4d577b448ef8b05b3e5ebb20280fa9fa3164f2fb046c52d5477c8bd579c8a4', '[\"*\"]', NULL, NULL, '2023-09-26 15:14:02', '2023-09-26 15:14:02'),
+(34, 'App\\Models\\User', 3, 'main', 'b8842a2fdffa7dc1fbc1873e2f56514bbbd2bf702728267217222f9bfeaef3a9', '[\"*\"]', '2023-09-26 17:42:03', NULL, '2023-09-26 15:21:28', '2023-09-26 17:42:03');
 
 -- --------------------------------------------------------
 
@@ -237,13 +235,24 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `category_id`, `title`, `slug`, `description`, `meta_title`, `meta_keywords`, `meta_description`, `original_price`, `selling_price`, `brand`, `image`, `quantity`, `status`, `popular`, `featured`, `created_at`, `updated_at`) VALUES
-(1, 11, 'a', 'a', 'a', 'a', 'a', 'a', '123', '123', 'a', 'uploads/products/64fc4a4c76267.jpg', '13', 1, 1, 1, '2023-09-02 15:42:10', '2023-09-09 07:34:52'),
-(2, 10, 'a', 'a', 'a', 'a', 'a', 'a', '120', '100', 'a', 'uploads/products/64fc4a6567f3a.jpg', '124', 1, 1, 1, '2023-09-02 16:00:07', '2023-09-09 07:35:17'),
-(3, 11, 'shirt', 'shirt', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, animi!', 'a', 'a', 'a', '160', '120', 'zara', 'uploads/products/64fc4a80c6bdb.jpg', '44', 1, 1, 1, '2023-09-02 16:00:57', '2023-09-09 17:17:37'),
-(4, 10, 'h', 'h', 'h', 'h', 'h', 'h', '12', '12', 'h', 'uploads/products/64fc4a92cd0a0.jpg', '0', 0, 0, 0, '2023-09-04 15:51:11', '2023-09-09 17:12:29'),
-(5, 7, 'c', 'c', 'c', 'c', 'c', 'c', '1222', '121212', 'c', 'uploads/products/64fc4aaa64ac9.jpg', '3', 0, 0, 0, '2023-09-04 15:53:49', '2023-09-09 17:12:29'),
-(6, 7, 'w', 'w', 'www', 'w', 's', 'w', '12', '12', 'w', 'uploads/products/64fc4ab9e346f.jpg', '12', 0, 0, 0, '2023-09-04 16:02:16', '2023-09-09 07:36:41'),
-(7, 11, 'r', 'r', 'r', 'r', 'r', 'r', '41', '41', 'r', 'uploads/products/64f63a596a45d.jpg', '41', 0, 0, 0, '2023-09-04 16:02:51', '2023-09-04 17:13:13');
+(8, 7, 'Lorem ipsum dolor sit amet.', 'jeans', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ab debitis aspernatur quia est molestias quibusdam dolores ut error officiis.', 'Meta Title', 'Meta Keyword', 'Meta Description', '120', '100', 'zara', 'uploads/products/64ff7ecfb53a8.jpg', '15', 0, 1, 1, '2023-09-11 17:55:43', '2023-09-11 17:55:43'),
+(9, 7, 'ex iusto harum au', 'jeans', 'oluta in illum, consequuntur ullam et rem enim voluptates aut nemo quasi ipsum consequatur delectus blanditiis, ducimus minus? Eos molestiae earum consequatu', 'Meta Title', 'Meta Keyword', 'Meta Description', '150', '130', 'gap', 'uploads/products/64ff7f74ce78e.jpg', '25', 0, 1, 1, '2023-09-11 17:58:28', '2023-09-11 17:58:28'),
+(10, 7, 'voluptatem cum re', 'jeans', 'iste dolor nulla, fugit soluta! Quae qui sequi, eligendi aliquam beatae earum, maiores magnam amet nesciunt accusamus mollitia nulla minima porro quibusdam', 'Meta Title', 'Meta Keyword', 'Meta Description', '250', '200', 'zara', 'uploads/products/64ff7fa671c0f.jpg', '24', 0, 1, 1, '2023-09-11 17:59:18', '2023-09-11 17:59:18'),
+(11, 10, 'voluptatem cum reprehenderit', 't-shirt', 'consequuntur ullam et rem enim voluptates aut nemo quasi ipsum consequatur delectus blanditiis, ducimus minus? Eos molestiae earum consequatur', 'Meta Title', 'Meta Keyword', 'Meta Description', '600', '450', 'zara', 'uploads/products/65046a847c3e6.jpg', '45', 0, 1, 1, '2023-09-15 11:30:28', '2023-09-15 11:30:28'),
+(12, 10, 'aliquid in blanditiis', 't-shirt', 'cumque officia laborum eveniet tempora! Dicta aliquid, explicabo sunt ipsam laudantium earum eaque exercitationem nam nesciunt voluptatem cum', 'Meta Title', 'Meta Keyword', 'Meta Description', '570', '500', 'h&m', 'uploads/products/65046ac8c0efe.jpg', '90', 0, 1, 0, '2023-09-15 11:31:36', '2023-09-15 11:31:36'),
+(13, 10, 'repellendus ratione rem', 't-shirt', 'Natus quidem voluptatem est repellendus laboriosam dicta error laborum sed vel quae, dolores iusto beatae nesciunt vero illum reiciendis quam', 'Meta Title', 'Meta Keyword', 'Meta Description', '780', '670', 'bershka', 'uploads/products/65046b2f5995d.jpg', '55', 0, 0, 1, '2023-09-15 11:33:19', '2023-09-15 11:33:19'),
+(14, 11, 'ibus deserunt architecto', 'shirts', 'Laborum fugiat temporibus optio ipsum odio sit doloremque minus quia, aliquam voluptates necessitatibus ipsa nostrum non ab aperiam cumque debitis illo', 'Meta Title', 'Meta Keyword', 'Meta Description', '400', '340', 'beneshty', 'uploads/products/650465a1cafd3.jpg', '34', 0, 1, 0, '2023-09-15 11:09:37', '2023-09-15 11:09:37'),
+(15, 11, 'quod qui cupiditate', 'shirts', 'totam vitae. Perferendis voluptas nesciunt nobis corrupti, quidem et similique quas a illum', 'Meta Title', 'Meta Keyword', 'Meta Description', '450', '400', 'Hermès', 'uploads/products/6504669036c59.jpg', '23', 0, 0, 1, '2023-09-15 11:13:36', '2023-09-15 11:13:36'),
+(16, 11, 'illo dolore mollitia', 'shirts', 'ducimus minus? Eos molestiae earum consequatur ut tempore blanditiis amet, suscipit deleniti minus nulla adipisci possimus sit repellendus ratione rem, voluptates dolorum cupiditate', 'Meta Title', 'Meta Keyword', 'Meta Description', '780', '700', 'Cartier', 'uploads/products/650466e47389b.jpg', '78', 0, 1, 1, '2023-09-15 11:15:00', '2023-09-15 11:15:00'),
+(17, 15, 'cumque debitis illo', 'sports', 'consequuntur ullam et rem enim voluptates aut nemo quasi ipsum consequatur delectus blanditiis, ducimus minus? Eos molestiae earum consequatur ut', 'Meta Title', 'Meta Keyword', 'Meta Description', '400', '230', 'Cartier', 'uploads/products/6504676b001a1.jpg', '23', 0, 1, 1, '2023-09-15 11:17:15', '2023-09-15 11:17:15'),
+(18, 15, 'reiciendis ex iusto', 'sports', 'tempora ipsa nemo corrupti. Error ad officiis quis maxime autem consequatur nemo quidem esse earum! Natus quidem voluptatem est repellendus laboriosam dicta error laborum', 'Meta Title', 'Meta Keyword', 'Meta Description', '800', '670', 'Adidas', 'uploads/products/650467b1c9b55.jpg', '120', 0, 0, 0, '2023-09-15 11:18:25', '2023-09-15 11:18:25'),
+(19, 15, 'Perferendis voluptas nesciunt', 'sports', 'dis ex iusto harum aut beatae quod rem! Magni omnis accusantium sapiente? Dolorem culpa vero similique perferendis molestiae aliquid in blanditiis, illum n', 'Meta Title', 'Meta Keyword', 'Meta Description', '120', '110', 'Fabindia', 'uploads/products/65131a815bf0a.jpg', '22', 1, 1, 1, '2023-09-26 14:53:05', '2023-09-26 14:53:05'),
+(20, 16, 'aboriosam di', 'sofa', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, provident. Eligend', 'Meta Title', 'Meta Keyword', 'Meta Description', '780', '700', 'Flying Machine', 'uploads/products/65131ad0bdaf8.jpg', '567', 0, 0, 1, '2023-09-26 14:54:24', '2023-09-26 14:54:24'),
+(21, 16, 'm et similique q', 'sofa', 'uasi ipsum consequatur delectus blanditiis, ducimus minus? Eos molestiae earum consequatur ut tempore blanditiis amet, suscipit deleniti minus nulla adipisci possimus sit repellendus ratione rem, voluptates dolorum cupiditate temporibus imp', NULL, NULL, NULL, '560', '400', 'Louis Philippe', 'uploads/products/65131b0374ac9.jpg', '45', 0, 0, 0, '2023-09-26 14:55:15', '2023-09-26 14:55:15'),
+(22, 16, 'nulla quod qui cupiditate', 'sofa', 'Excepturi hic iusto omnis odit ad cumque officia laborum eveniet tempora! Dicta aliquid, explicabo sunt ipsam laudantium earum eaque exercitationem nam nesciunt', 'Meta Title', 'Meta Keyword', 'Meta Description', '800', '680', 'Cartier', 'uploads/products/65131b3bd21c5.jpg', '50', 0, 1, 0, '2023-09-26 14:56:11', '2023-09-26 14:56:11'),
+(23, 17, 'temporibus optio', 'tv', 'emporibus optio ipsum odio sit doloremque minus quia, aliquam voluptates necessitatibus ipsa nostrum non ab aperiam cumque debitis illo dolore mollitia excepturi deserunt culpa? Quo architecto reprehenderit nulla quod qui cupiditate?', 'Meta Title', 'Meta Keyword', 'Meta Description', '7000', '6700', 'samsung', 'uploads/products/65131b6fb1fa9.jpg', '60', 0, 1, 1, '2023-09-26 14:57:03', '2023-09-26 14:57:03'),
+(25, 17, 'temporibus impedit', 'tv', 'eritatis aliquam laboriosam officiis nemo, reiciendis ex iusto harum aut beatae quod rem! Magni omnis accusantium sapiente? Dolorem culpa vero similique perferendis molestiae aliquid in blanditiis, illum nihil sit repudiandae accusamus qua', 'Meta Title', 'Meta Keyword', 'Meta Description', '6000', '5800', 'lg', 'uploads/products/65131c3215422.jpg', '50', 0, 0, 1, '2023-09-26 15:00:18', '2023-09-26 15:00:18'),
+(26, 17, 'Quae qui sequi', 'tv', 'or nulla, fugit soluta! Quae qui sequi, eligendi aliquam beatae earum, maiores magnam amet nesciunt accusamus mollitia nulla minima porro quibusdam, cumque volupt', NULL, NULL, NULL, '4000', '3400', 'toshiba', 'uploads/products/65131c669d336.jpg', '56', 0, 0, 1, '2023-09-26 15:01:10', '2023-09-26 15:01:10');
 
 -- --------------------------------------------------------
 
@@ -347,13 +356,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -383,13 +392,13 @@ ALTER TABLE `order_items`
 -- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `users`
